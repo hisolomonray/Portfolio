ef498e15d7ce48f356ba4beb7b14a3765816c2bf
Solomon Ray <hisolomonray@gmail.com>
2019-10-30 19:49:54 -0700

    l

--- src/pages/form.js
+++ src/pages/form.js
@@ -14,20 +14,7 @@ function form() {
             Your Email: <input type="email" name="email" />
           </label>
         </p>
-        <p>
-          <label>
-            Your Role:{" "}
-            <select name="role[]" multiple>
-              <option value="leader">Leader</option>
-              <option value="follower">Follower</option>
-            </select>
-          </label>
-        </p>
-        <p>
-          <label>
-            Message: <textarea name="message"></textarea>
-          </label>
-        </p>
+
         <p>
           <button type="submit">Send</button>
         </p>

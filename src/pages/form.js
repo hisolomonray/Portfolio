8bc9a9d2286a9bd736187c864db85877fd9a5661
Solomon Ray <hisolomonray@gmail.com>
2019-10-30 19:58:48 -0700

    change

--- src/pages/form.js
+++ src/pages/form.js
@@ -3,7 +3,14 @@ import React from "react"
 function form() {
   return (
     <div>
-      <form name="contact" method="POST" data-netlify="true">
+      <form
+        name="contact"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        {/* You still need to add the hidden input with the form name to your JSX form */}
+        <input type="hidden" name="form-name" value="contact" />>
         <p>
           <label>
             Your Name: <input type="text" name="name" />
@@ -14,7 +21,6 @@ function form() {
             Your Email: <input type="email" name="email" />
           </label>
         </p>
-
         <p>
           <button type="submit">Send</button>
         </p>

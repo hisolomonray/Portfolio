17f2a366c1c6190f3e5fba134c8f6edb93befc04
Solomon Ray <hisolomonray@gmail.com>
2019-11-06 14:31:32 -0800

    m

--- src/pages/form.js
+++ src/pages/form.js
@@ -11,7 +11,12 @@ function form() {
         data-netlify-honeypot="bot-field"
       >
         {/* You still need to add the hidden input with the form name to your JSX form */}
-        <input type="hidden" name="form-name" value="contact" />
+        <input
+          type="hidden"
+          data-netlify-recaptcha="true"
+          name="form-name"
+          value="contact"
+        />
         <p>
           <label>
             <input type="text" name="name" placeholder="Name" />
@@ -25,7 +30,7 @@ function form() {
         <p>
           <textarea type="text" name="text" placeholder="Message"></textarea>
         </p>
-        <div data-netlify-recaptcha="true"></div>
+        <div data-netlify-recaptcha="true"> </div>
         <p>
           <button type="submit">Send</button>
         </p>

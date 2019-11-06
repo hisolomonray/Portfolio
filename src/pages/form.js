90265be7556e8cdd35115a4c87c84ed36139b123
Solomon Ray <hisolomonray@gmail.com>
2019-11-06 11:57:46 -0800

    m

--- src/pages/form.js
+++ src/pages/form.js
@@ -19,7 +19,7 @@ function form() {
         data-netlify-honeypot="bot-field"
       >
         {/* You still need to add the hidden input with the form name to your JSX form */}
-        <input type="hidden" name="form-name" value="contact" />
+
         <p>
           <label>
             <input type="text" name="name" placeholder="Name" />

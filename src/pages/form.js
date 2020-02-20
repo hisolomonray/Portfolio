4dda8bda2b315fe84c7baa48601eaea36639b74b
Solomon Ray <hisolomonray@gmail.com>
2020-02-19 19:49:32 -0800

    commited

--- src/pages/form.js
+++ src/pages/form.js
@@ -3,42 +3,7 @@ import { MdPerson, MdEmail, MdKeyboardHide } from "react-icons/md"
 function form() {
   return (
     <div>
-      <form
-        action="/thank/"
-        name="contact"
-        method="POST"
-        data-netlify-recaptcha="true"
-        netlify-honeypot="bot-field"
-        data-netlify="true"
-      >
-        {/* You still need to add the hidden input with the form name to your JSX form */}
-        <p class="hidden">
-          <label>
-            Don’t fill this out if you're human: <input name="bot-field" />
-          </label>
-        </p>
-        <p>
-          <label>
-            <MdPerson size="2rem" />{" "}
-            <input type="text" name="name" placeholder="Name" />
-          </label>
-        </p>
-
-        <p>
-          <label>
-            <MdEmail size="2rem" />
-            <input type="email" name="email" placeholder="Email" />
-          </label>
-        </p>
-        <p>
-          <MdKeyboardHide size="2rem" />
-          <textarea type="text" name="text" placeholder="Message"></textarea>
-        </p>
-        <div data-netlify-recaptcha="true"></div>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
+      <h1>Contact </h1>
     </div>
   )
 }
